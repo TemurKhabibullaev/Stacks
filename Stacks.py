f5a0b4295711159d62e98e9af0f625d8ac72ba82
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-03-04 14:04:34 -0500

    In process

--- Stacks.py
+++ Stacks.py
@@ -1,13 +1,33 @@
-from LinkedList import SinglyLinkedList
-instance = SinglyLinkedList()
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 
 class Stacks:
     def __init__(self):
-        self.head = instance.head()
+        self.head = None
 
     def push(self, data):
-        instance.add_head(data)
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
     def pop(self):
-        instance.del_head()
+        ret = self.head.data
+        self.head = self.head.next
+        return ret
+
+    def display(self):
+        elems = []
+        current = self.head
+        elems.append(current.data)
+        while current.next:
+            current = current.next
+            elems.append(current.data)
+        print(elems)
+
+stack_instance = Stacks
+stack_instance.push(4)
